6d7889150b669bb293743057a266642ba0dbb7bc
raohassan4212 <raohassan4212@gmail.com>
2026-01-07 19:36:06 +0500

    remove port

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'backend.cosmeticchemist.com',
-        port: '3001',
+        port: '',
         pathname: '/api/media/**',
       },
     ],
